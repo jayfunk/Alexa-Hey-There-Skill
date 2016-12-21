5f978b846275d01da433e6499b244958fa9e0601
Tyler Cook <Tyler.Cook@theice.com>
2016-12-21 16:32:00 -0500

    Some minor re-organization Added Amazon.HelpIntent Added welcome message speech

--- heyThereLambda.js
+++ heyThereLambda.js
@@ -9,57 +9,6 @@
  * http://amzn.to/1LGWsLG
  */
 
-
-// --------------- Helpers that build all of the responses -----------------------
-
-function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
-    return {
-        outputSpeech: {
-            type: 'PlainText',
-            text: output,
-        },
-        card: {
-            type: 'Simple',
-            title: `SessionSpeechlet - ${title}`,
-            content: `SessionSpeechlet - ${output}`,
-        },
-        reprompt: {
-            outputSpeech: {
-                type: 'PlainText',
-                text: repromptText,
-            },
-        },
-        shouldEndSession,
-    };
-}
-
-function buildResponse(sessionAttributes, speechletResponse) {
-    return {
-        version: '1.0',
-        sessionAttributes,
-        response: speechletResponse,
-    };
-}
-
-
-// --------------- Functions that control the skill's behavior -----------------------
-
-function getWelcomeResponse(callback) {
-    // If we wanted to initialize the session to have some attributes we could add those here.
-    const sessionAttributes = {};
-    const cardTitle = 'Welcome';
-    const speechOutput = 'Welcome to the Alexa Skills Kit sample. ' +
-        'Please tell me your favorite color by saying, my favorite color is red';
-    // If the user either does not reply to the welcome message or says something that is not
-    // understood, they will be prompted again with this text.
-    const repromptText = 'Please tell me your favorite color by saying, ' +
-        'my favorite color is red';
-    const shouldEndSession = false;
-
-    callback(sessionAttributes,
-        buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
-}
-
 function handleSessionEndRequest(callback) {
     const cardTitle = 'Session Ended';
     const speechOutput = 'Thank you for trying the Alexa Skills Kit sample. Have a nice day!';
@@ -75,9 +24,6 @@ function createFavoriteColorAttributes(favoriteColor) {
     };
 }
 
-/**
- * Sets the color in the session and prepares the speech to reply to the user.
- */
 function setColorInSession(intent, session, callback) {
     const cardTitle = intent.name;
     const favoriteColorSlot = intent.slots.Color;
@@ -128,98 +74,128 @@ function getColorFromSession(intent, session, callback) {
          buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
 }
 
-
-// --------------- Events -----------------------
-
-/**
- * Called when the session starts.
- */
-function onSessionStarted(sessionStartedRequest, session) {
-    console.log(`onSessionStarted requestId=${sessionStartedRequest.requestId}, sessionId=${session.sessionId}`);
-}
-
-/**
- * Called when the user launches the skill without specifying what they want.
- */
-function onLaunch(launchRequest, session, callback) {
-    console.log(`onLaunch requestId=${launchRequest.requestId}, sessionId=${session.sessionId}`);
-
-    // Dispatch to your skill's launch.
-    getWelcomeResponse(callback);
-}
-
-/**
- * Called when the user specifies an intent for this skill.
- */
-function onIntent(intentRequest, session, callback) {
-    console.log(`onIntent requestId=${intentRequest.requestId}, sessionId=${session.sessionId}`);
-
-    const intent = intentRequest.intent;
-    const intentName = intentRequest.intent.name;
-
-    // Dispatch to your skill's intent handlers
-    if (intentName === 'MyColorIsIntent') {
-        setColorInSession(intent, session, callback);
-    } else if (intentName === 'WhatsMyColorIntent') {
-        getColorFromSession(intent, session, callback);
-    } else if (intentName === 'AMAZON.HelpIntent') {
-        getWelcomeResponse(callback);
-    } else if (intentName === 'AMAZON.StopIntent' || intentName === 'AMAZON.CancelIntent') {
-        handleSessionEndRequest(callback);
-    } else {
-        throw new Error('Invalid intent');
-    }
-}
-
-/**
- * Called when the user ends the session.
- * Is not called when the skill returns shouldEndSession=true.
- */
-function onSessionEnded(sessionEndedRequest, session) {
-    console.log(`onSessionEnded requestId=${sessionEndedRequest.requestId}, sessionId=${session.sessionId}`);
-    // Add cleanup logic here
-}
-
-
 // --------------- Main handler -----------------------
 
 // Route the incoming request based on type (LaunchRequest, IntentRequest,
 // etc.) The JSON body of the request is provided in the event parameter.
 exports.handler = (event, context, callback) => {
-    try {
-        console.log(`event.session.application.applicationId=${event.session.application.applicationId}`);
+  try {
+    console.log(`event.session.application.applicationId=${event.session.application.applicationId}`);
 
-        /**
-         * Uncomment this if statement and populate with your skill's application ID to
-         * prevent someone else from configuring a skill that sends requests to this function.
-         */
-        /*
-        if (event.session.application.applicationId !== 'amzn1.echo-sdk-ams.app.[unique-value-here]') {
-             callback('Invalid Application ID');
-        }
-        */
-
-        if (event.session.new) {
-            onSessionStarted({ requestId: event.request.requestId }, event.session);
-        }
-
-        if (event.request.type === 'LaunchRequest') {
-            onLaunch(event.request,
-                event.session,
-                (sessionAttributes, speechletResponse) => {
-                    callback(null, buildResponse(sessionAttributes, speechletResponse));
-                });
-        } else if (event.request.type === 'IntentRequest') {
-            onIntent(event.request,
-                event.session,
-                (sessionAttributes, speechletResponse) => {
-                    callback(null, buildResponse(sessionAttributes, speechletResponse));
-                });
-        } else if (event.request.type === 'SessionEndedRequest') {
-            onSessionEnded(event.request, event.session);
-            callback();
-        }
-    } catch (err) {
-        callback(err);
+    if (event.session.new) {
+      console.log(`onSessionStarted requestId=${event.request.requestId}, sessionId=${event.session.sessionId}`);
     }
+    /**
+     * Uncomment this if statement and populate with your skill's application ID to
+     * prevent someone else from configuring a skill that sends requests to this function.
+     */
+    /*
+    if (event.session.application.applicationId !== 'amzn1.echo-sdk-ams.app.[unique-value-here]') {
+      callback('Invalid Application ID');
+    }
+    */
+    switch(event.request.type) {
+      case 'LaunchRequest':
+        onLaunch(
+          event.request,
+          event.session,
+          (sessionAttributes, speechletResponse) => {
+            callback(null, buildResponse(sessionAttributes, speechletResponse));
+          }
+        );
+        break;
+      case 'IntentRequest':
+        onIntent(
+          event.request,
+          event.session,
+          (sessionAttributes, speechletResponse) => {
+            callback(null, buildResponse(sessionAttributes, speechletResponse));
+          }
+        );
+        break;
+      case 'SessionEndedRequest':
+        onSessionEnded(event.request, event.session);
+        callback();
+        break;
+      default:
+        callback(`Unknown request type ${event.request.type}`);
+        break;
+    }
+  } catch (err) {
+    callback(err);
+  }
 };
+
+function onLaunch(launchRequest, session, callback) {
+  console.log(`onLaunch requestId=${launchRequest.requestId}, sessionId=${session.sessionId}`);
+  getWelcomeResponse(callback);
+}
+
+function getWelcomeResponse(callback) {
+  // If we wanted to initialize the session to have some attributes we could add those here.
+  const sessionAttributes = {};
+
+  const cardTitle = 'Hey There!';
+  const firstExampleText = 'Start by saying, send a message to Danielle';
+  const speechOutput = `Hey There! is a simple tool allowing you to send messages to a preconfigured set of friends. ${firstExampleText}`;
+  const shouldEndSession = false;
+
+  callback(
+    sessionAttributes,
+    buildSpeechletResponse(cardTitle, speechOutput, firstExampleText, shouldEndSession)
+  );
+}
+
+function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+  return {
+    outputSpeech: {
+      type: 'PlainText',
+      text: output,
+    },
+    card: {
+      type: 'Simple',
+      title: `SessionSpeechlet - ${title}`,
+      content: `SessionSpeechlet - ${output}`,
+    },
+    reprompt: {
+      outputSpeech: {
+        type: 'PlainText',
+        text: repromptText,
+      },
+    },
+    shouldEndSession,
+  };
+}
+
+function buildResponse(sessionAttributes, speechletResponse) {
+  return {
+    version: '1.0',
+    sessionAttributes,
+    response: speechletResponse,
+  };
+}
+
+function onIntent(intentRequest, session, callback) {
+  console.log(`onIntent requestId=${intentRequest.requestId}, sessionId=${session.sessionId}`);
+
+  const intent = intentRequest.intent;
+  const intentName = intentRequest.intent.name;
+
+  // Dispatch to your skill's intent handlers
+  if (intentName === 'MyColorIsIntent') {
+    setColorInSession(intent, session, callback);
+  } else if (intentName === 'WhatsMyColorIntent') {
+    getColorFromSession(intent, session, callback);
+  } else if (intentName === 'AMAZON.HelpIntent') {
+    getWelcomeResponse(callback);
+  } else if (intentName === 'AMAZON.StopIntent' || intentName === 'AMAZON.CancelIntent') {
+    handleSessionEndRequest(callback);
+  } else {
+    throw new Error('Invalid intent');
+  }
+}
+
+function onSessionEnded(sessionEndedRequest, session) {
+  console.log(`onSessionEnded requestId=${sessionEndedRequest.requestId}, sessionId=${session.sessionId}`);
+  // Add cleanup logic here
+}
