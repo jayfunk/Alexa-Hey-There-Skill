79d8b0eb0a37526001b84185b3f6daf3a114d26a
Tyler Cook <Tyler.Cook@theice.com>
2016-12-21 19:02:44 -0500

    Removed example code to a separate file for archiving Added first intent to gather party information

---
+++ amazonExampleFunctions.js
@@ -0,0 +1,73 @@
+/**
+ * This sample demonstrates a simple skill built with the Amazon Alexa Skills Kit.
+ * The Intent Schema, Custom Slots, and Sample Utterances for this skill, as well as
+ * testing instructions are located at http://amzn.to/1LzFrj6
+ *
+ * For additional samples, visit the Alexa Skills Kit Getting Started guide at
+ * http://amzn.to/1LGWsLG
+ */
+
+function handleSessionEndRequest(callback) {
+    const cardTitle = 'Session Ended';
+    const speechOutput = 'Thank you for trying the Alexa Skills Kit sample. Have a nice day!';
+    // Setting this to true ends the session and exits the skill.
+    const shouldEndSession = true;
+
+    callback({}, buildSpeechletResponse(cardTitle, speechOutput, null, shouldEndSession));
+}
+
+function createFavoriteColorAttributes(favoriteColor) {
+    return {
+        favoriteColor,
+    };
+}
+
+function setColorInSession(intent, session, callback) {
+    const cardTitle = intent.name;
+    const favoriteColorSlot = intent.slots.Color;
+    let repromptText = '';
+    let sessionAttributes = {};
+    const shouldEndSession = false;
+    let speechOutput = '';
+
+    if (favoriteColorSlot) {
+        const favoriteColor = favoriteColorSlot.value;
+        sessionAttributes = createFavoriteColorAttributes(favoriteColor);
+        speechOutput = `I now know your favorite color is ${favoriteColor}. You can ask me ` +
+            "your favorite color by saying, what's my favorite color?";
+        repromptText = "You can ask me your favorite color by saying, what's my favorite color?";
+    } else {
+        speechOutput = "I'm not sure what your favorite color is. Please try again.";
+        repromptText = "I'm not sure what your favorite color is. You can tell me your " +
+            'favorite color by saying, my favorite color is red';
+    }
+
+    callback(sessionAttributes,
+         buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+}
+
+function getColorFromSession(intent, session, callback) {
+    let favoriteColor;
+    const repromptText = null;
+    const sessionAttributes = {};
+    let shouldEndSession = false;
+    let speechOutput = '';
+
+    if (session.attributes) {
+        favoriteColor = session.attributes.favoriteColor;
+    }
+
+    if (favoriteColor) {
+        speechOutput = `Your favorite color is ${favoriteColor}. Goodbye.`;
+        shouldEndSession = true;
+    } else {
+        speechOutput = "I'm not sure what your favorite color is, you can say, my favorite color " +
+            ' is red';
+    }
+
+    // Setting repromptText to null signifies that we do not want to reprompt the user.
+    // If the user does not respond or says something that is not understood, the session
+    // will end.
+    callback(sessionAttributes,
+         buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
+}
--- heyThereLambda.js
+++ heyThereLambda.js
@@ -1,83 +1,5 @@
 'use strict';
 
-/**
- * This sample demonstrates a simple skill built with the Amazon Alexa Skills Kit.
- * The Intent Schema, Custom Slots, and Sample Utterances for this skill, as well as
- * testing instructions are located at http://amzn.to/1LzFrj6
- *
- * For additional samples, visit the Alexa Skills Kit Getting Started guide at
- * http://amzn.to/1LGWsLG
- */
-
-function handleSessionEndRequest(callback) {
-    const cardTitle = 'Session Ended';
-    const speechOutput = 'Thank you for trying the Alexa Skills Kit sample. Have a nice day!';
-    // Setting this to true ends the session and exits the skill.
-    const shouldEndSession = true;
-
-    callback({}, buildSpeechletResponse(cardTitle, speechOutput, null, shouldEndSession));
-}
-
-function createFavoriteColorAttributes(favoriteColor) {
-    return {
-        favoriteColor,
-    };
-}
-
-function setColorInSession(intent, session, callback) {
-    const cardTitle = intent.name;
-    const favoriteColorSlot = intent.slots.Color;
-    let repromptText = '';
-    let sessionAttributes = {};
-    const shouldEndSession = false;
-    let speechOutput = '';
-
-    if (favoriteColorSlot) {
-        const favoriteColor = favoriteColorSlot.value;
-        sessionAttributes = createFavoriteColorAttributes(favoriteColor);
-        speechOutput = `I now know your favorite color is ${favoriteColor}. You can ask me ` +
-            "your favorite color by saying, what's my favorite color?";
-        repromptText = "You can ask me your favorite color by saying, what's my favorite color?";
-    } else {
-        speechOutput = "I'm not sure what your favorite color is. Please try again.";
-        repromptText = "I'm not sure what your favorite color is. You can tell me your " +
-            'favorite color by saying, my favorite color is red';
-    }
-
-    callback(sessionAttributes,
-         buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
-}
-
-function getColorFromSession(intent, session, callback) {
-    let favoriteColor;
-    const repromptText = null;
-    const sessionAttributes = {};
-    let shouldEndSession = false;
-    let speechOutput = '';
-
-    if (session.attributes) {
-        favoriteColor = session.attributes.favoriteColor;
-    }
-
-    if (favoriteColor) {
-        speechOutput = `Your favorite color is ${favoriteColor}. Goodbye.`;
-        shouldEndSession = true;
-    } else {
-        speechOutput = "I'm not sure what your favorite color is, you can say, my favorite color " +
-            ' is red';
-    }
-
-    // Setting repromptText to null signifies that we do not want to reprompt the user.
-    // If the user does not respond or says something that is not understood, the session
-    // will end.
-    callback(sessionAttributes,
-         buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
-}
-
-// --------------- Main handler -----------------------
-
-// Route the incoming request based on type (LaunchRequest, IntentRequest,
-// etc.) The JSON body of the request is provided in the event parameter.
 exports.handler = (event, context, callback) => {
   try {
     console.log(`event.session.application.applicationId=${event.session.application.applicationId}`);
@@ -85,15 +7,11 @@ exports.handler = (event, context, callback) => {
     if (event.session.new) {
       console.log(`onSessionStarted requestId=${event.request.requestId}, sessionId=${event.session.sessionId}`);
     }
-    /**
-     * Uncomment this if statement and populate with your skill's application ID to
-     * prevent someone else from configuring a skill that sends requests to this function.
-     */
-    /*
-    if (event.session.application.applicationId !== 'amzn1.echo-sdk-ams.app.[unique-value-here]') {
+
+    if (event.session.application.applicationId !== process.env.SKILL_ID) {
       callback('Invalid Application ID');
     }
-    */
+
     switch(event.request.type) {
       case 'LaunchRequest':
         onLaunch(
@@ -132,17 +50,14 @@ function onLaunch(launchRequest, session, callback) {
 }
 
 function getWelcomeResponse(callback) {
-  // If we wanted to initialize the session to have some attributes we could add those here.
-  const sessionAttributes = {};
-
   const cardTitle = 'Hey There!';
-  const firstExampleText = 'Start by saying, send a message to Danielle';
-  const speechOutput = `Hey There! is a simple tool allowing you to send messages to a preconfigured set of friends. ${firstExampleText}`;
+  const repromptText = 'Start by saying, send a message to Danielle';
+  const speechOutput = `Hey There! is a simple tool allowing you to send messages to a preconfigured set of friends. ${repromptText}`;
   const shouldEndSession = false;
 
   callback(
-    sessionAttributes,
-    buildSpeechletResponse(cardTitle, speechOutput, firstExampleText, shouldEndSession)
+    {},
+    buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession)
   );
 }
 
@@ -179,20 +94,44 @@ function onIntent(intentRequest, session, callback) {
   console.log(`onIntent requestId=${intentRequest.requestId}, sessionId=${session.sessionId}`);
 
   const intent = intentRequest.intent;
-  const intentName = intentRequest.intent.name;
+  const intentName = intent.name;
 
-  // Dispatch to your skill's intent handlers
-  if (intentName === 'MyColorIsIntent') {
-    setColorInSession(intent, session, callback);
-  } else if (intentName === 'WhatsMyColorIntent') {
-    getColorFromSession(intent, session, callback);
-  } else if (intentName === 'AMAZON.HelpIntent') {
-    getWelcomeResponse(callback);
-  } else if (intentName === 'AMAZON.StopIntent' || intentName === 'AMAZON.CancelIntent') {
-    handleSessionEndRequest(callback);
-  } else {
-    throw new Error('Invalid intent');
+  switch (intentName) {
+    case 'HeyThere':
+      promptForMessageResponse(intent, callback);
+      break;
+    case 'AMAZON.HelpIntent':
+      getWelcomeResponse(callback);
+      break;
+    default:
+      throw new Error('Invalid Intent');
+      break;
   }
+}
+
+function promptForMessageResponse(intent, callback) {
+  const sessionAttributes = createSessionAttributesFromSlots(intent);
+
+  const cardTitle = 'Hey There!';
+  const repromptText = 'What\'s the message?';
+  const speechOutput = `Alright, sending a message to ${sessionAttributes.to} from ${sessionAttributes.from}. ${repromptText}`;
+  const shouldEndSession = false;
+
+  callback(
+    sessionAttributes,
+    buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession)
+  );
+}
+
+function createSessionAttributesFromSlots(intent) {
+  const toName = intent.slots.To.value;
+  const fromName = intent.slots.From.value;
+
+  return {
+    to: toName,
+    from: fromName,
+    message: '',
+  };
 }
 
 function onSessionEnded(sessionEndedRequest, session) {
